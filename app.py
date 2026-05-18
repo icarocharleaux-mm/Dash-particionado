ab61b4ffd85771b8516b4e2524da8fbbd8d57d4b
icarocharleaux-mm <icarocharleaux@gmail.com>
2026-05-18 14:55:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ authenticator = stauth.Authenticate(
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
-    config['pre-authorized']
 )
 
 # Renderiza o formulário de login no centro da tela

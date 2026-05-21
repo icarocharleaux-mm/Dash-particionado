cfc3a2f669d67a0a0d65c6c54aaafa931fd01043
icarocharleaux-mm <icarocharleaux@gmail.com>
2026-05-21 10:15:48 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import logging
 
+@st.cache_data(ttl=600)
 def load_data():
     mapa_meses_num = {
         1: 'Jan', 2: 'Fev', 3: 'Mar', 4: 'Abr', 5: 'Mai', 6: 'Jun',
